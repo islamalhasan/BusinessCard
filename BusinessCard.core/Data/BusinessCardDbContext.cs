d9bda9125522d565904af309ce6d8fdb6d14b4e8
islam alhasan <islamalhasan8@gmail.com>
2024-10-04 15:22:02 +0300

    Add Setings Configration

---
+++ BusinessCard.core/Data/BusinessCardDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard.core.Data
+{
+    public class BusinessCardDbContext:DbContext
+    {
+        public BusinessCardDbContext(DbContextOptions options) :base(options)
+        {
+
+        }
+        public DbSet<User> User { get; set; }
+
+    }
+}
--- BusinessCard/Program.cs
+++ BusinessCard/Program.cs
@@ -1,8 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using BusinessCard.core.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
 
+var builder = WebApplication.CreateBuilder(args);
+var ConnectionString = builder.Configuration.GetConnectionString("BusinessCardDbConnectionString");
+if (ConnectionString == null)
+{
+    // Handle the case where the connection string is not found
+    Console.WriteLine("Connection string not found.");
+}
+else
+{
+    // Attempt to open a connection to the database
+    using (var connection = new SqlConnection(ConnectionString))
+    {
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connection opened successfully.");
+            // Connection is open, you can perform your database operations here
+
+            // Always close the connection when done
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            // Handle any errors that occur when trying to open the connection
+            Console.WriteLine("An error occurred while trying to open the connection: " + ex.Message);
+        }
+    }
+}
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BusinessCardDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString(ConnectionString)));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
